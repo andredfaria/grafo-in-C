9235cef223e7ac4c0b86fcf9c66f2507ec98d22c
Ricardo Saraiva <ricardo.saraiva@avance.inf.br>
2019-11-13 15:12:57 -0300

    Refencia dos algoritimos para implementar

--- grafos.c
+++ grafos.c
@@ -1,3 +1,5 @@
+// https://www.ime.usp.br/~pf/algoritmos_para_grafos/
+
 #include <stdio.h>
 
 int main(){
@@ -12,7 +14,7 @@ int main(){
     }
 
 
-    prinf("A matriz é ...");
+    prinf("A matriz ï¿½ ...");
     for(i=0;i<5;i++){
         for(j=0;j<5;j++){
             printf("%d ", grafo[i][j]);
@@ -29,6 +31,37 @@ int main(){
 
 
     if(graficoNULO == 25)
-        printf("O grafico é nulo !");
+        printf("O grafico ï¿½ nulo !");
 
+    
+}
+
+
+
+// vertex pa[1000];
+// int dist[1000];
+
+void GRAPHsptD0( Graph G, vertex s) 
+{ 
+   for (vertex v = 0; v < G->V; ++v) 
+      pa[v] = -1;
+   pa[s] = s, dist[s] = 0; 
+
+   while (true) {
+      int min = INFINITY;
+      vertex x, y;
+      for (vertex v = 0; v < G->V; ++v) {
+         if (pa[v] == -1) continue;
+         for (link a = G->adj[v]; a != NULL; a = a->next) {
+            if (pa[a->w] != -1) continue;
+            if (dist[v] + a->cst < min) {
+               min = dist[v] + a->cst;
+               x = v, y = a->w;
+            }
+         }
+      }
+      if (min == INFINITY) // A 
+         break; // B
+      pa[y] = x, dist[y] = min;
+   }
 }
